3cfba470ae2c1f26d3efb5e034131a1c7c4b642c
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-12-14 15:30:47 +0300

    "Complete Category Page"

--- my-app/src/lib/category.svelte
+++ my-app/src/lib/category.svelte
@@ -1,38 +1,23 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
 	import { P, A, Span } from 'flowbite-svelte';
 	export let src = '$lib/assets/placeholder.jpeg';
-	export let cname='Foods and Beverages'
-
+	export let color;
+	export let cname = '';
 </script>
 
-<div class="gradient">
-	<div class="category-container" >
-		<h1>{cname}</h1>
-		<img {src}>
-	</div>
+<div
+	class="category-container"
+	class:blue={color === 'blue'}
+	class:green={color === 'green'}
+	class:red={color === 'red'}
+>
+	<h1>{cname}</h1>
+	<img {src} />
 </div>
 
-
 <style>
-
-	.gradient {
-		position: relative;
-		width: 100%;
-		height: 100%;
-	}
-
-	.gradient:before {
-		content: '';
-		position: relative;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		
-		z-index: -1; /* Place the pseudo-element behind the .category-container */
-	}
 	.category-container {
 		display: flex;
 		max-width: var(--max-w-sm, 384px);
@@ -42,23 +27,40 @@
 		gap: var(--0, 0px);
 		flex: 1 0 0;
 		align-self: stretch;
+		justify-content: space-between;
 
 		border-radius: var(--rounded-lg, 8px);
 		border-top: 1px solid var(--gray-200, #e5e7eb);
 		border-right: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
 		border-bottom: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
 		border-left: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
-		background: linear-gradient(157deg, rgba(28, 100, 242, 0.00) 36.24%, #0E3583 82.97%), var(--blue-500, #3f83f8) 50% / cover no-repeat;
-		
 		/* shadow-md */
 		box-shadow:
 			0px 2px 4px -2px rgba(0, 0, 0, 0.05),
-			0px 4px 6px -1px rgba(0, 0, 0, 0.1);	
+			0px 4px 6px -1px rgba(0, 0, 0, 0.1);
 	}
 
-	h1{
+	.blue {
+		background:
+			linear-gradient(157deg, rgba(28, 100, 242, 0) 36.24%, #0e3583 82.97%),
+			var(--blue-500, #3f83f8) 50% / cover no-repeat;
+	}
+
+	.green {
+		background:
+			linear-gradient(157deg, rgba(14, 159, 110, 0) 36.24%, #086c4b 82.97%),
+			var(--green-500, #0e9f6e) 50% / cover no-repeat;
+	}
+
+	.red {
+		background:
+			linear-gradient(157deg, rgba(242, 28, 28, 0) 36.24%, #830e0e 82.97%),
+			var(--red-500, #f05252) 50% / cover no-repeat;
+	}
+
+	h1 {
 		align-self: stretch;
-		color: var(--white, #FFF);
+		color: var(--white, #fff);
 
 		/* leading-tight/text-3xl/font-bold */
 		font-family: Inter;
@@ -66,5 +68,15 @@
 		font-style: normal;
 		font-weight: 700;
 		line-height: 125%; /* 37.5px */
+	}
+
+	img {
+		position: relative;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		max-width: 400px;
+		min-width: 250px;
+		height: auto;
 	}
 </style>
