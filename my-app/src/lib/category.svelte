97f1581aa9445a890c7f362f1b47c04d1f5c6081
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-12-14 14:51:19 +0300

    "Category Component"

--- my-app/src/lib/category.svelte
+++ my-app/src/lib/category.svelte
@@ -1,15 +1,38 @@
 <script>
+// @ts-nocheck
+
 	import { P, A, Span } from 'flowbite-svelte';
-	export let src = '';
+	export let src = '$lib/assets/placeholder.jpeg';
+	export let cname='Foods and Beverages'
+
 </script>
 
-<div class="category-container" style="">
-	<div class="gradient">
-		<img {src} />
+<div class="gradient">
+	<div class="category-container" >
+		<h1>{cname}</h1>
+		<img {src}>
 	</div>
 </div>
 
+
 <style>
+
+	.gradient {
+		position: relative;
+		width: 100%;
+		height: 100%;
+	}
+
+	.gradient:before {
+		content: '';
+		position: relative;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		
+		z-index: -1; /* Place the pseudo-element behind the .category-container */
+	}
 	.category-container {
 		display: flex;
 		max-width: var(--max-w-sm, 384px);
@@ -25,12 +48,12 @@
 		border-right: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
 		border-bottom: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
 		border-left: var(--Boxed, 1px) solid var(--gray-200, #e5e7eb);
-		background: var(--blue-500, #3f83f8);
-
+		background: linear-gradient(157deg, rgba(28, 100, 242, 0.00) 36.24%, #0E3583 82.97%), var(--blue-500, #3f83f8) 50% / cover no-repeat;
+		
 		/* shadow-md */
 		box-shadow:
 			0px 2px 4px -2px rgba(0, 0, 0, 0.05),
-			0px 4px 6px -1px rgba(0, 0, 0, 0.1);
+			0px 4px 6px -1px rgba(0, 0, 0, 0.1);	
 	}
 
 	h1{
@@ -43,5 +66,5 @@
 		font-style: normal;
 		font-weight: 700;
 		line-height: 125%; /* 37.5px */
-	}	
+	}
 </style>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -37,11 +37,28 @@
     
 </script>
 
+
+	
+
+	
+
 <main class="w-full px-8 flex-col justify-start items-start gap-8 inline-flex">
-	<h1 class="font-sans text-black text-5xl font-semibold font-['Inter'] leading-10">
-		Popular Categories
-	</h1>
-	<section></section>
+	<section class="category-container">
+		<h1 class="font-sans text-black text-5xl font-semibold font-['Inter'] leading-10">
+			Popular Categories
+		</h1>
+		<section class="category-cards">
+			<Category src={FB}/>
+		</section>
+		<div class="more-categories">
+			<a style="display: flex; align-items:center; gap: var(--2, 8px);" href="'/">
+				<span class="more-categories-text">More Categories</span>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+					<path d="M12.6222 9.06666V12.5035C12.6223 12.6125 12.6009 12.7204 12.5592 12.8211C12.5175 12.9219 12.4564 13.0134 12.3793 13.0905C12.3022 13.1675 12.2107 13.2287 12.11 13.2703C12.0093 13.312 11.9013 13.3334 11.7923 13.3333H3.49649C3.38749 13.3334 3.27953 13.312 3.17881 13.2703C3.07808 13.2287 2.98656 13.1675 2.90948 13.0905C2.8324 13.0134 2.77128 12.9219 2.72961 12.8211C2.68793 12.7204 2.66653 12.6125 2.66663 12.5035V4.20763C2.66653 4.09863 2.68793 3.99067 2.72961 3.88995C2.77128 3.78922 2.8324 3.6977 2.90948 3.62062C2.98656 3.54354 3.07808 3.48242 3.17881 3.44075C3.27953 3.39908 3.38749 3.37767 3.49649 3.37777H6.7804M9.2316 2.66666H13.3333V6.76834M7.72334 8.27661L13.2544 2.74559" stroke="#1A56DB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+				</svg>
+			</a>
+		</div>	
+	</section>
 	<h1 class="font-sans text-black text-5xl font-semibold font-['Inter'] leading-10">
 		Today's Sales
 	</h1>
@@ -54,6 +71,9 @@
 			{/each}
 		{/await}
 	</section>
+
+	
+
 </main>
 
 <style>
@@ -65,6 +85,7 @@
 	.category-container {
 		display: flex;
 		justify-content: space-between;
+		flex-direction: column;
 		align-items: flex-start;
 		align-content: flex-start;
 		row-gap: 32px;
@@ -72,5 +93,36 @@
 		flex-wrap: wrap;
 		margin-left: 20px;
 		margin-right: 20px;
+	}
+
+	.category-cards{
+		display: flex;
+		height: 257px;
+		align-items: flex-start;
+		gap: 32px;
+		align-self: stretch;
+		flex-wrap: wrap;
+		border-radius: var(--Boxed, 0px);
+	}
+
+	.more-categories{
+		display: flex;
+		width: 100% ;
+		justify-content: flex-end;
+		align-items: flex-start;
+		gap: 10px;
+		align-self: stretch;
+		border-radius: var(--Boxed, 0px);
+	}
+
+	.more-categories-text{
+		color: var(--primary-700, #1A56DB);
+
+		/* leading-tight/text-base/font-normal */
+		font-family: Inter;
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 125%; /* 20px */
 	}
 </style>
